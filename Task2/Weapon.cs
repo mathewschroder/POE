d97f27405f71c49efcd096ebc3f1bc4b392d67f6
mathewschroder <schrodermat007@gmail.com>
2020-11-30 12:38:34 +0200

    Add project files.

---
+++ Task2/Weapon.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task2
+{
+    abstract class Weapon : Item
+    {
+        protected int damage;
+        protected int range;
+        protected int durability;
+        protected int cost;
+        protected string weaponType;
+        public int damageValue
+        {
+            get { return damage; }
+            set { damage = damageValue; }
+        }
+        public int rangeValue
+        {
+            get { return range; }
+            set { range = rangeValue; }
+        }
+        public int durabilityValue
+        {
+            get { return durability; }
+            set { durability = durabilityValue; }
+        }
+        public int costValue
+        {
+            get { return cost; }
+            set { cost = costValue; }
+        }
+
+        public string weaponTypeValue 
+        {
+            get { return weaponType; }
+            set { weaponType = weaponTypeValue; }
+        }
+
+
+        public Weapon (int xValue, int yValue, int damageValue, int rangeValue, int durabilityValue, int costValue, string weaponTypeValue) :base (xValue, yValue) 
+        {
+            
+        }
+    }
+}
